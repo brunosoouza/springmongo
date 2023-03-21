23494068bd82c4ab32a28c89e9b0af8512fbdaeb
brn <gsbrunosouza@gmail.com>
2023-03-21 20:10:16 -0300

    obtendo post por id

---
+++ src/main/java/com/brn/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.brn.workshopmongo.services;
+
+import com.brn.workshopmongo.domain.Post;
+import com.brn.workshopmongo.repositorys.PostRepository;
+import com.brn.workshopmongo.services.exception.ObjectNofFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> findById = repo.findById(id);
+        if(!findById.isPresent()){
+            throw new ObjectNofFoundException("Objeto não encontrado");
+        }
+        return findById.get();
+    }
+}
